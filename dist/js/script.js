227578ae544ed54e196758f190edc0b6fe3d2cba
ZRShamim <zrshamim8822@gmail.com>
2021-07-15 18:32:42 +0600

    processing

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,11 @@ const openMenu = document.getElementById('hamburger');
 const closeMenu = document.getElementById('close');
 const navList = document.getElementById('nav-list');
 const toggle = document.getElementById('toggle');
-
+const btnLearn = document.getElementById('btn-learn');
+const btnFree = document.getElementById('btn-free');
+const btnLogin = document.getElementById('btn-login');
+const btnSign = document.getElementById('btn-sign');
+const arrowRotaion = document.getElementById('arrow');
 openMenu.addEventListener('click', () => {
   navList.classList.add('showmenu');
   navList.classList.remove('hidden');
@@ -17,6 +21,27 @@ closeMenu.addEventListener('click', () => {
   closeMenu.classList.add('hidden');
 });
 
-// toggle.addEventListener('click', () => {
-//   document.getElementById('toggle-menu').classList.toggle('hidden');
-// });
+toggle.addEventListener('click', () => {
+  document.getElementById('toggle-menu').classList.toggle('hidden');
+  arrowRotaion.classList.toggle('arrow-rot');
+});
+
+btnLearn.addEventListener('click', () => {
+  btnLearn.classList.add('btn-main-active');
+  btnFree.classList.remove('btn-main-active');
+});
+
+btnFree.addEventListener('click', () => {
+  btnLearn.classList.remove('btn-main-active');
+  btnFree.classList.add('btn-main-active');
+});
+
+btnLogin.addEventListener('click', () => {
+  btnLogin.classList.add('btn-primary-active');
+  btnSign.classList.remove('btn-primary-active');
+});
+
+btnSign.addEventListener('click', () => {
+  btnLogin.classList.remove('btn-primary-active');
+  btnSign.classList.add('btn-primary-active');
+});
