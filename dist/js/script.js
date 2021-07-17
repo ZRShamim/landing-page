bf997548aebb2ee429af90c33a8fda98112dc96f
ZRShamim <zrshamim8822@gmail.com>
2021-07-17 12:50:18 +0600

    Tunning

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,31 +1,42 @@
+const openclose = document.getElementById('nav-toggle');
 const openMenu = document.getElementById('hamburger');
 const closeMenu = document.getElementById('close');
 const navList = document.getElementById('nav-list');
-const toggle = document.getElementById('toggle');
+
+const toggle1 = document.getElementById('toggle-1');
+const toggle2 = document.getElementById('toggle-2');
+const toggle3 = document.getElementById('toggle-3');
+
 const btnLearn = document.getElementById('btn-learn');
 const btnFree = document.getElementById('btn-free');
 const btnLogin = document.getElementById('btn-login');
 const btnSign = document.getElementById('btn-sign');
-const arrowRotaion = document.getElementById('arrow');
-openMenu.addEventListener('click', () => {
-  navList.classList.add('showmenu');
-  navList.classList.remove('hidden');
-  openMenu.classList.add('hidden');
-  closeMenu.classList.remove('hidden');
+const arrowRotation = document.getElementById('arrow');
+
+// Main Menu Open-Close
+openclose.addEventListener('click', () => {
+  closeMenu.classList.toggle('hidden');
+  openMenu.classList.toggle('hidden');
+  navList.classList.toggle('show-menu');
 });
 
-closeMenu.addEventListener('click', () => {
-  navList.classList.remove('showmenu');
-  navList.classList.add('hidden');
-  openMenu.classList.remove('hidden');
-  closeMenu.classList.add('hidden');
+// submenu
+toggle1.addEventListener('click', () => {
+  document.getElementById('toggle-menu-1').classList.toggle('hidden');
+  // arrowRotation.classList.toggle('arrow-rot');
 });
 
-toggle.addEventListener('click', () => {
-  document.getElementById('toggle-menu').classList.toggle('hidden');
-  arrowRotaion.classList.toggle('arrow-rot');
+toggle2.addEventListener('click', () => {
+  document.getElementById('toggle-menu-2').classList.toggle('hidden');
+  // arrowRotation.classList.toggle('arrow-rot');
 });
 
+toggle3.addEventListener('click', () => {
+  document.getElementById('toggle-menu-3').classList.toggle('hidden');
+  // arrowRotation.classList.toggle('arrow-rot');
+});
+
+// btn Dynamic
 btnLearn.addEventListener('click', () => {
   btnLearn.classList.add('btn-main-active');
   btnFree.classList.remove('btn-main-active');
@@ -34,14 +45,4 @@ btnLearn.addEventListener('click', () => {
 btnFree.addEventListener('click', () => {
   btnLearn.classList.remove('btn-main-active');
   btnFree.classList.add('btn-main-active');
-});
-
-btnLogin.addEventListener('click', () => {
-  btnLogin.classList.add('btn-primary-active');
-  btnSign.classList.remove('btn-primary-active');
-});
-
-btnSign.addEventListener('click', () => {
-  btnLogin.classList.remove('btn-primary-active');
-  btnSign.classList.add('btn-primary-active');
 });
